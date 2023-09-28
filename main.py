8c89fc189f3a991636e4cf9b50b6e01101f1fc38
mono <mercadogmatias@gmail.com>
2023-09-28 20:41:29 -0300

    Separate websocket logic service

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 
 from frontend.html.ui import HTML
-from services import connection_manager
+from services.connection_manager import start_chat
+import uvicorn
 
-app = FastAPI()
+app: FastAPI = FastAPI()
 
 
 @app.get("/")
-async def get():
+async def get() -> HTMLResponse:
+    """Returns a simple html ui to interact with the app"""
     return HTMLResponse(HTML)
 
 
-@app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
-    await connection_manager.connect(websocket)
-    try:
-        while True:
-            data: str = await websocket.receive_text()
-            await connection_manager.send_personal_message(f"You wrote: {data}", websocket)
-            await connection_manager.broadcast(f"Client #{client_id} says: {data}")
-    except WebSocketDisconnect:
-        connection_manager.disconnect(websocket)
-        await connection_manager.broadcast(f"Client #{client_id} has left the chat")
+@app.websocket("/ws/{client_id:int}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int) -> None:
+    """Handles the websocket connection broadcasting the received text"""
+    await start_chat(websocket=websocket, client_id=client_id)
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", log_level="info")
--- services/connection_manager.py
+++ services/connection_manager.py
@@ -1,21 +1,38 @@
-from fastapi import WebSocket
+from fastapi import WebSocket, WebSocketDisconnect
 
 active_connections: list[WebSocket] = []
 
 
-async def connect(websocket: WebSocket):
+async def connect(websocket: WebSocket) -> None:
+    """Accepts websocket connection"""
     await websocket.accept()
     active_connections.append(websocket)
 
 
-def disconnect(websocket: WebSocket):
+def disconnect(websocket: WebSocket) -> None:
+    """Removes a given websocket from the pool"""
     active_connections.remove(websocket)
 
 
-async def send_personal_message(message: str, websocket: WebSocket):
+async def send_personal_message(message: str, websocket: WebSocket) -> None:
+    """Sends text to the given websocket"""
     await websocket.send_text(message)
 
 
-async def broadcast(message: str):
+async def broadcast(message: str) -> None:
+    """Sends text to all active connections"""
     for connection in active_connections:
         await connection.send_text(message)
+
+
+async def start_chat(websocket: WebSocket, client_id: int) -> None:
+    """Establishes websocket connection and broadcasts chat messages"""
+    await connect(websocket)
+    try:
+        while True:
+            data: str = await websocket.receive_text()
+            await send_personal_message(f"You wrote: {data}", websocket)
+            await broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        disconnect(websocket)
+        await broadcast(f"Client #{client_id} has left the chat")
